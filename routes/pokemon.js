8f32b392b36d09d9f52501f8381b739dd1e6d946
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-23 05:58:08 -0700

    HEAD: First commit

---
+++ routes/pokemon.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+// GET /pokemon - return a page with favorited Pokemon
+router.get('/', function(req, res) {
+
+  // 1.1 Model Association, notice no " ' ' " around `pokemon`
+  db.pokemon.findAll()
+  .then( favorites => {
+    console.log('🪀 🪀 🪀 GET /: ', favorites)
+    res.render('pokemon/index', {
+      pokemon: favorites
+    })
+  })
+  .catch(error => console.log(`❌ Error`, error));
+});
+
+
+router.get('/:name', function(req, res) {
+  // TODO: Get all records from the DB and render to view
+  
+  console.log(`🦄: req.params`, req.params.name);
+});
+
+// POST /pokemon - receive the name of a pokemon and add it to the database
+router.post('/', function(req, res) {
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then( digimon => {
+    console.log('💎 💎 💎 post /: ', digimon)
+    res.redirect('/pokemon')
+  });
+});
+
+module.exports = router;
