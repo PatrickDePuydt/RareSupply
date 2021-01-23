47d98bef43cd328171405d8377e73862cdded43f
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-23 06:42:53 -0700

    main: Raze migrations, keeps, invest in new styles

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname, '/public')));
 
-// GET / - main index of site
 app.get('/', (req, res) => {
   let apiURL = 'http://pokeapi.co/api/v2/pokemon/';
   
-  // Use request to call the API
   axios.get(apiURL).then(apiResponse => {
     let responseResults = apiResponse.data.results;
     res.render('index', { pearls: responseResults.slice(0, 151) });
   })
+
 });
 
 app.get('/pearls', (req, res) => {
