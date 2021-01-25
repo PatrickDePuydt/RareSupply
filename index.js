7f4c98d5a57fd3b2eddea96958f22ad7e9fa99fd
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-25 11:34:38 -0700

    main: move over api call

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const path = require('path');
 const app = express();
@@ -12,14 +11,7 @@ app.use(ejsLayouts);
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  const question = "object?medium=2028216&place=2028213"
-  const accessKey = process.env.API_KEY;
-  const apiURL = `https://api.harvardartmuseums.org/${question}&apikey=${accessKey}`;
-
-  axios.get(apiURL).then(apiResponse => {
-    let responseResults = apiResponse.data.records;
-    res.render('index', {PAYLOAD: responseResults});
-  })
+  res.render('index')
 });
 
 app.use('/treasure', require('./routes/treasure'));
--- routes/treasure.js
+++ routes/treasure.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios'); 
+
 
 let seedImage = ["https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150", "https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150"]
 
 router.get('/', (req, res) => {
-  res.render('treasure', {seedImage})
+    const question = "object?medium=2028216&place=2028213"
+  const accessKey = process.env.API_KEY;
+  const apiURL = `https://api.harvardartmuseums.org/${question}&apikey=${accessKey}`;
+
+  axios.get(apiURL).then(apiResponse => {
+    let responseResults = apiResponse.data.records;
+    res.render('treasure', {PAYLOAD: responseResults});
+  })
+
 })
 
 module.exports = router;
