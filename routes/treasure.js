d2041dd9aeb7742e4acd44fdd7936b87db5d24c5
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-25 11:58:08 -0700

    main: something is working ...

--- routes/treasure.js
+++ routes/treasure.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 let seedImage = ["https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150", "https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150"]
 
 router.get('/', (req, res) => {
-    const question = "object?medium=2028216&place=2028213"
+  const question = "object?medium=2028216&place=2028213"
   const accessKey = process.env.API_KEY;
   const apiURL = `https://api.harvardartmuseums.org/${question}&apikey=${accessKey}`;
 
@@ -16,5 +16,20 @@ router.get('/', (req, res) => {
   })
 
 })
+
+router.get('/:id', (req, res) => {
+  
+  const id = req.params.id
+  const accessKey = process.env.API_KEY;
+  const apiURL = `https://api.harvardartmuseums.org/image/${id}?apikey=${accessKey}`;
+
+  axios.get(apiURL).then(apiResponse => {
+    // console.log(`☀️ API Response: `, apiResponse)
+    let responseResults = apiResponse.data;
+
+    res.render('treasure/image', {PAYLOAD: responseResults});
+  })
+  
+});
 
 module.exports = router;
