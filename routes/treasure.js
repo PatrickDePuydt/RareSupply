bed149d11fc0d409671654a09c126e6610d86837
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-25 14:56:26 -0700

    main: Rebrand names

--- routes/treasure.js
+++ routes/treasure.js
@@ -6,10 +6,10 @@ const axios = require('axios');
 let seedImage = ["https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150", "https://nrs.harvard.edu/urn-3:HUAM:77963_dynmc?height=150&width=150"]
 
 router.get('/', (req, res) => {
-  const baseConstraint = "object"
-  const question = "medium=2028216&place=2028213"
+  const resource = "object"
+  const filter = "medium=2028216&place=2028213"
   const accessKey = process.env.API_KEY;
-  const apiURL = `https://api.harvardartmuseums.org/${baseConstraint}?${question}&apikey=${process.env.API_KEY}`;
+  const apiURL = `https://api.harvardartmuseums.org/${resource}?${filter}&apikey=${process.env.API_KEY}`;
 
   axios.get(apiURL).then(apiResponse => {
     let responseResults = apiResponse.data.records;
