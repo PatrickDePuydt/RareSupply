19c461ddfb63ca467cd8b9bf11cbb036717b7998
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-26 17:05:38 -0700

    main: Update test associations

--- test-associations.js
+++ test-associations.js
@@ -1,10 +1,11 @@
 const db = require('./models');
-// Create treasure
 
 db.treasure.findOrCreate({
-  where: {
-    image_url: "https://nrs.harvard.edu/urn-3:HUAM:48875_dynmc?height=300&width=300",
-    user_id: 1
-  }
-})
-// Create an associated item
+    where: {
+        image: 'https://nrs.harvard.edu/urn-3:HUAM:CARP08828_dynmc?width=300&height=300',
+        userId: 1
+    },
+    defaults: {
+        userId: 1
+    }
+});
