2d8d2763f2c3b6a649a8caeb4f7ab3fa0cddc8fc
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-26 17:22:17 -0700

    main: progress

--- models/treasure.js
+++ models/treasure.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.treasure.blongsTo(models.user),
+      models.treasure.hasMany(models.tag, {through: "treasureTags"})
     }
   };
   treasure.init({
