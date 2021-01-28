be9711efc3568156f450d2b0deebe67b3f1e89f2
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-28 10:31:57 -0700

    main: add auth dependencies to index

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
-const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+const helmet = require('helmet');
+const passport = require('../config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const path = require('path'); // Local File Path for Js/CSS
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(ejsLayouts);
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(ejsLayouts);
+app.use(helmet());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
 
 app.get('/', (req, res) => {
   res.redirect('/auth/login'); // Send the user immediately to login
 });
 
-app.get('/profile', (req,res) => {
-  res.render('profile');
-});
+app.use('/auth', require('./routes/auth')); // Login/Signup
+app.use('/treasure', require('./routes/treasure')); // Most app routes
 
-app.use('/auth', require('./routes/auth'));
-app.use('/treasure', require('./routes/treasure'));
-
-
-app.listen(port, () => {
-  console.log('...listening on', port );
+app.listen(process.env.PORT || 3000, () => {
+  console.log('...listening on', process.env.PORT || 3000 );
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const passport = require('../config/ppConfig');
 
 
 router.get('/signup', function(req, res) {
