f70cdd11a329ba56f38075eed351b3ce60fbcd71
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-28 09:46:57 -0700

    main: Begin porting over auth

--- models/user.js
+++ models/user.js
@@ -1,22 +1,58 @@
 'use strict';
+const bcrypt = require('bcrypt');
+
 const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+ 
     static associate(models) {
       models.user.hasMany(models.treasure)
     }
+
+    validPassword(typedPassword) {
+      let isValid = bcrypt.compareSync(typedPassword, this.password);
+      return isValid;
+    }
+
+    toJSON() {
+      let userData = this.get();
+      delete userData.password;
+      return userData;
+    }
   };
   user.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password msut be between 8 and 99 characters'
+        },
+        notContains: {
+          arts: this.name,
+          msg: 'Password cannot contain your name'
+        }
+      }
+    }
+  },{
+    hooks: {
+      beforeCreate: (pendingUser) => {
+        if (pendingUser && pendingUser.password) {
+          let hash = bcrypt.hashSync(pendingUser.password, 12);
+          pendingUser.password = hash
+        }
+      }
+    },
     sequelize,
     modelName: 'user',
   });
